a81694001af5af1981118a07cca428616a1ca7ed
okuzmychov <o.kuzmichev@gmail.com>
2023-09-24 22:52:16 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,31 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout/Layout';
 import { Loader } from 'components/Loader/Loader';
 
+const Home = lazy(() => import('./pages/Home/Home.jsx'));
+const MovieDetails = lazy(() => import('./pages/MovieDetails/MovieDetails'));
+const Movies = lazy(() => import('./pages/Movies/Movies'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
+
 export const App = () => {
   return (
     <div>
-      React homework template
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/goit-react-hw-05-movies" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+            <Route path="movies/:movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+          </Route>
+          <Route
+            path="*"
+            element={<Navigate to={'/goit-react-hw-05-movies'} replace />}
+          />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
